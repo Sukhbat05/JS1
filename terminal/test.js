d06f228a66bb30f0b0ff5d2adeaec1f840a31d5d
Sukhbat05 <asukhbat27@gmail.com>
2025-08-07 21:58:44 +0800

    zasalt08.08.1

--- terminal/test.js
+++ terminal/test.js
@@ -1,1 +1,10 @@
-products.filter((product) => {});
+const myNode = document.getElementById("MyNode");
+myNode.innerText = "Hello DOM !";
+
+document.querySelector("span").style.fontSize = "x-large";
+document.querySelector("span").style.color = "red";
+
+ const para = document.createElement("p")
+
+ para.innerText = "This is paragraph";
+ document.getElementById("myList").appendChild(para)
